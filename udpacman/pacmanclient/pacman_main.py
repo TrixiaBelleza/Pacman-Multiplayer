b97ef0264c3c0e425aff86e4609096246016086a
Trix <trbelleza@up.edu.ph>
2018-11-30 12:45:48 +0800

    UDP GUI. Bug: map matrix passed was blank

---
+++ udpacman/pacmanclient/pacman_main.py
@@ -0,0 +1,76 @@
+import pacmanserver.server
+import pacmanclient.pacman_client
+import random
+from pygame.locals import *
+import pygame
+import pong.pacman_entities
+import tkinter
+from tkinter import *
+
+pygame.init()
+address = input("Server address (host:port) = ")
+host, port = address.split(':')
+port = int(port)
+
+SERVER_ADDRESS = (host, port)
+# SCREEN_SIZE = (pong.pacman_entities.World.WIDTH, pong.pacman_entities.World.HEIGHT)
+
+def key_listeners(event):
+	player_xpos = pong.pacman_entities.Pacman.get_xPos(pong.pacman_entities.Pacman)
+	player_ypos = pong.pacman_entities.Pacman.get_yPos(pong.pacman_entities.Pacman) 
+	map_matrix = pong.pacman_entities.World.get_map_matrix(pong.pacman_entities.World)
+	print("MAP MATRIX")
+	print(map_matrix)
+	if event.keysym == "Left":
+		print("Entered left")
+		if map_matrix[player_xpos][player_ypos-1] == "D" or map_matrix[player_xpos][player_ypos-1] == "s" or map_matrix[player_xpos][player_ypos-1] == "e":
+			map_matrix[player_xpos][player_ypos] = "e"
+			map_matrix[player_xpos][player_ypos-1] = "P"
+			pong.pacman_entities.World.set_canvas(player_ypos*20, player_xpos*20, player_ypos*20+20, player_xpos*20+20, "BLACK")
+			pong.pacman_entities.World.set_canvas((player_ypos-1)*20, player_xpos*20, (player_ypos-1)*20+20, player_xpos*20+20, "RED")
+			# block = canvas.create_rectangle(player_ypos*20, player_xpos*20, player_ypos*20+20, player_xpos*20+20, fill="BLACK", outline="")
+			# block = canvas.create_rectangle((player_ypos-1)*20, player_xpos*20, (player_ypos-1)*20+20, player_xpos*20+20, fill="RED", outline="")
+	# elif event.keysym == "Right":
+	# 	if map_matrix[player_xpos][player_ypos+1] == "D" or map_matrix[player_xpos][player_ypos+1] == "s" or map_matrix[player_xpos][player_ypos+1] == "e":
+	# 		map_matrix[player_xpos][player_ypos] = "e"
+	# 		map_matrix[player_xpos][player_ypos+1] = "P"
+	# 		block = canvas.create_rectangle(player_ypos*20, player_xpos*20, player_ypos*20+20, player_xpos*20+20, fill="BLACK", outline="")
+	# 		block = canvas.create_rectangle((player_ypos+1)*20, player_xpos*20, (player_ypos+1)*20+20, player_xpos*20+20, fill="RED", outline="")
+	# elif event.keysym == "Up":
+	# 	if map_matrix[player_xpos-1][player_ypos] == "D" or map_matrix[player_xpos-1][player_ypos] == "s" or map_matrix[player_xpos-1][player_ypos] == "e":
+	# 		map_matrix[player_xpos][player_ypos] = "e"
+	# 		map_matrix[player_xpos-1][player_ypos] = "P"
+	# 		block = canvas.create_rectangle(player_ypos*20, player_xpos*20, player_ypos*20+20, player_xpos*20+20, fill="BLACK", outline="")
+	# 		block = canvas.create_rectangle(player_ypos*20, (player_xpos-1)*20, player_ypos*20+20, (player_xpos-1)*20+20, fill="RED", outline="")
+	# elif event.keysym == "Down":
+	# 	if map_matrix[player_xpos+1][player_ypos] == "D" or map_matrix[player_xpos+1][player_ypos] == "s" or map_matrix[player_xpos+1][player_ypos] == "e":
+	# 		map_matrix[player_xpos][player_ypos] = "e"
+	# 		map_matrix[player_xpos+1][player_ypos] = "P"
+	# 		block = canvas.create_rectangle(player_ypos*20, player_xpos*20, player_ypos*20+20, player_xpos*20+20, fill="BLACK", outline="")
+	# 		block = canvas.create_rectangle(player_ypos*20, (player_xpos+1)*20, player_ypos*20+20, (player_xpos+1)*20+20, fill="RED", outline="")
+
+
+def main():
+
+
+	#Randomly generate the address for this client
+	local_address = ('localhost', random.randint(10000, 20000))
+
+	server_handler = pacmanclient.pacman_client.ServerHandler(local_address,
+														SERVER_ADDRESS
+														)
+	server_handler.start()
+	
+
+	world = pong.pacman_entities.World("map1.txt")
+	window = world.pacman_window()
+	window.resizable(width=FALSE, height=FALSE)
+	global pacman
+	pacman = pong.pacman_entities.Pacman("yellow", "player", world)
+	window.mainloop()
+		
+
+	return
+
+if __name__ == '__main__':
+	main()
---
+++ udpacman/start_server.py
@@ -0,0 +1,9 @@
+import pacmanserver.main
+
+
+def main():
+	pacmanserver.main.main()
+
+
+if __name__ == '__main__':
+	main()
