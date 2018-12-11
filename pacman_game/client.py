99fec82637b17bcd512f51a9a57b546361bc3fd5
kjhycluna <kcluna@up.edu.ph>
2018-12-11 18:38:20 +0800

    Edited UI for movements

--- pacman_game/client.py
+++ pacman_game/client.py
@@ -17,6 +17,8 @@ class Client():
 	port = ''
 	client_addr = ''
 	map_matrix = []
+	window = ''
+	canvas = ''
 	#Bale gagawin natin na equivalent si CLIENT and PLAYER
 	def __init__(self, player_name, player_type):
 		self.player = pacman.Player(player_name, player_type)
@@ -64,6 +66,18 @@ class Client():
 
 		return(loaded_data.player_count)
 
+	def startGame(self):
+		startGamePacket =  UDPpacket.UDPpacket("START_GAME")
+		self.socket.sendto(pickle.dumps(startGamePacket), self.server_address)	
+
+		#Receive game map
+		#Receive player count from server
+		data, addr = self.socket.recvfrom(self.BUFFER_SIZE)
+		loaded_data = pickle.loads(data)
+
+		self.map_matrix = loaded_data.map_matrix
+		self.render_window()
+
 	def move(self, movement): 
 		movementPacket = UDPpacket.UDPpacket("MOVE")
 		movementPacket.movement = movement
@@ -75,91 +89,114 @@ class Client():
 		loaded_data = pickle.loads(data)
 
 		self.map_matrix = loaded_data.map_matrix
-		self.render_board()
+		self.renew_map()
 
-	def render_board(self):
+	def render_window(self):
+		self.window = self.pacman_window()
+		self.window.resizable(width=FALSE, height=FALSE)
+		self.game_map()
+		self.window.mainloop()
 
-		def pacman_window():
-			tk_window = tkinter.Tk()
-			tk_window.title("BATTLE OF THE PACMEN")
-			w = 500 # height for tk_window
-			h = 600 # height for tk_window
-			x = (tk_window.winfo_screenwidth()/2) - (w/2) # calculate x and y coordinates for tk_window
-			y = (tk_window.winfo_screenheight()/2) - (h/2)
-			# set the dimensions of the screen and where it is placed
-			tk_window.geometry('%dx%d+%d+%d' % (w, h, x, y))
-			tk_window.configure(bg="BLACK", padx=50, pady=50)
-			return tk_window
+	def pacman_window(self):
+		tk_window = tkinter.Tk()
+		tk_window.title("BATTLE OF THE PACMEN")
+		w = 500 # height for tk_window
+		h = 600 # height for tk_window
+		x = (tk_window.winfo_screenwidth()/2) - (w/2) # calculate x and y coordinates for tk_window
+		y = (tk_window.winfo_screenheight()/2) - (h/2)
+		# set the dimensions of the screen and where it is placed
+		tk_window.geometry('%dx%d+%d+%d' % (w, h, x, y))
+		tk_window.configure(bg="BLACK", padx=50, pady=50)
+		return tk_window
 
-		def main():
-			window = pacman_window()
-			return window
+	def game_map(self):
+		global map_Frame
+		map_Frame = Frame(self.window, bg="BLACK", pady=20)
+		map_Frame.pack()
+		self.create_Map()
+	
+	def key_listeners(self, event):
+		print(event.keysym)
+		self.move(event.keysym)
 
-		def game_map():
-			global map_Frame
-			map_Frame = Frame(window, bg="BLACK", pady=20)
-			map_Frame.pack()
-			create_Map()
+	def create_Map(self):
+		block_height = 20 * len(self.map_matrix)
+		block_width = 20 * len(self.map_matrix[0])
+		self.canvas = tkinter.Canvas(map_Frame, bg="BLACK", height=block_height, width=block_width)
+		self.canvas.pack()
 		
-		def key_listeners(event):
-			print(event.keysym)
-			self.move(event.keysym)
-
-		def create_Map():
-			block_height = 20 * len(self.map_matrix)
-			block_width = 20 * len(self.map_matrix[0])
-			global canvas
-			canvas = tkinter.Canvas(map_Frame, bg="BLACK", height=block_height, width=block_width)
-			canvas.pack()
-			
-			y_pos = 0 # starting pixel in canvas
-			increment = 20 # to determine next position
-			for row in range(len(self.map_matrix)):
-				x_pos = 0
-				for col in range(len(self.map_matrix[row])):
-					if self.map_matrix[row][col] == "D":		# PAC-DOT
-						block = canvas.create_oval(x_pos+8, y_pos+8, x_pos-8+increment, y_pos-8+increment, fill="WHITE", outline="")
-					elif self.map_matrix[row][col] == "s":	# STAR
-						ax = x_pos+(increment/2), 
-						ay = y_pos
-						bx = x_pos+1
-						by = y_pos+(increment/3)
-						cx = x_pos+(increment/5)
-						cy = y_pos+increment
-						dx = x_pos+(increment/5)*4
-						dy = y_pos+increment
-						ex = x_pos+increment
-						ey = y_pos+(increment/3)
-						block = canvas.create_polygon(ax, ay, cx, cy, ex, ey, bx, by, dx, dy, fill="YELLOW", outline="")
-					elif self.map_matrix[row][col] == "w":	# WALL
-						block = canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="BLUE", outline="")
-					elif self.map_matrix[row][col] == "e":	# EMPTY FLOOR
-						block = canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="BLACK", outline="")
-					elif self.map_matrix[row][col] == "P":	# PLAYER
-						block = canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="RED", outline="")
-					x_pos += increment
-				y_pos += increment
-			
-			canvas.bind_all("<KeyPress-Up>", key_listeners) # binds event listener to whole canvas
-			canvas.bind_all("<KeyPress-Down>", key_listeners) # binds event listener to whole canvas
-			canvas.bind_all("<KeyPress-Left>", key_listeners) # binds event listener to whole canvas
-			canvas.bind_all("<KeyPress-Right>", key_listeners) # binds event listener to whole canvas
+		y_pos = 0 # starting pixel in self.canvas
+		increment = 20 # to determine next position
+		for row in range(len(self.map_matrix)):
+			x_pos = 0
+			for col in range(len(self.map_matrix[row])):
+				if self.map_matrix[row][col] == "D":		# PAC-DOT
+					block = self.canvas.create_oval(x_pos+8, y_pos+8, x_pos-8+increment, y_pos-8+increment, fill="WHITE", outline="")
+				elif self.map_matrix[row][col] == "s":	# STAR
+					ax = x_pos+(increment/2), 
+					ay = y_pos
+					bx = x_pos+1
+					by = y_pos+(increment/3)
+					cx = x_pos+(increment/5)
+					cy = y_pos+increment
+					dx = x_pos+(increment/5)*4
+					dy = y_pos+increment
+					ex = x_pos+increment
+					ey = y_pos+(increment/3)
+					block = self.canvas.create_polygon(ax, ay, cx, cy, ex, ey, bx, by, dx, dy, fill="YELLOW", outline="")
+				elif self.map_matrix[row][col] == "w":	# WALL
+					block = self.canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="BLUE", outline="")
+				elif self.map_matrix[row][col] == "e":	# EMPTY FLOOR
+					block = self.canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="BLACK", outline="")
+				elif self.map_matrix[row][col] == "P":	# PLAYER
+					block = self.canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="RED", outline="")
+				x_pos += increment
+			y_pos += increment
 		
-		window = main()
-		window.resizable(width=FALSE, height=FALSE)
-		game_map()
-		window.mainloop()
+		self.canvas.bind_all("<KeyPress-Up>", self.key_listeners) # binds event listener to whole self.canvas
+		self.canvas.bind_all("<KeyPress-Down>", self.key_listeners) # binds event listener to whole self.canvas
+		self.canvas.bind_all("<KeyPress-Left>", self.key_listeners) # binds event listener to whole self.canvas
+		self.canvas.bind_all("<KeyPress-Right>", self.key_listeners) # binds event listener to whole self.canvas
 
+	def renew_map(self):
+		block_height = 20 * len(self.map_matrix)
+		block_width = 20 * len(self.map_matrix[0])
+		self.canvas.pack_forget()
+		self.canvas = tkinter.Canvas(map_Frame, bg="BLACK", height=block_height, width=block_width)
+		self.canvas.pack()
+		
+		y_pos = 0 # starting pixel in self.canvas
+		increment = 20 # to determine next position
+		for row in range(len(self.map_matrix)):
+			x_pos = 0
+			for col in range(len(self.map_matrix[row])):
+				if self.map_matrix[row][col] == "D":		# PAC-DOT
+					block = self.canvas.create_oval(x_pos+8, y_pos+8, x_pos-8+increment, y_pos-8+increment, fill="WHITE", outline="")
+				elif self.map_matrix[row][col] == "s":	# STAR
+					ax = x_pos+(increment/2), 
+					ay = y_pos
+					bx = x_pos+1
+					by = y_pos+(increment/3)
+					cx = x_pos+(increment/5)
+					cy = y_pos+increment
+					dx = x_pos+(increment/5)*4
+					dy = y_pos+increment
+					ex = x_pos+increment
+					ey = y_pos+(increment/3)
+					block = self.canvas.create_polygon(ax, ay, cx, cy, ex, ey, bx, by, dx, dy, fill="YELLOW", outline="")
+				elif self.map_matrix[row][col] == "w":	# WALL
+					block = self.canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="BLUE", outline="")
+				elif self.map_matrix[row][col] == "e":	# EMPTY FLOOR
+					block = self.canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="BLACK", outline="")
+				elif self.map_matrix[row][col] == "P":	# PLAYER
+					block = self.canvas.create_rectangle(x_pos, y_pos, x_pos+increment, y_pos+increment, fill="RED", outline="")
+				x_pos += increment
+			y_pos += increment
+		
+		self.canvas.bind_all("<KeyPress-Up>", self.key_listeners) # binds event listener to whole self.canvas
+		self.canvas.bind_all("<KeyPress-Down>", self.key_listeners) # binds event listener to whole self.canvas
+		self.canvas.bind_all("<KeyPress-Left>", self.key_listeners) # binds event listener to whole self.canvas
+		self.canvas.bind_all("<KeyPress-Right>", self.key_listeners) # binds event listener to whole self.canvas
+		
 
-	def startGame(self):
-		startGamePacket =  UDPpacket.UDPpacket("START_GAME")
-		self.socket.sendto(pickle.dumps(startGamePacket), self.server_address)	
-
-		#Receive game map
-		#Receive player count from server
-		data, addr = self.socket.recvfrom(self.BUFFER_SIZE)
-		loaded_data = pickle.loads(data)
-
-		self.map_matrix = loaded_data.map_matrix
-		self.render_board()
 
